3f80249a11960374c69124f3ffa263491de8baf7
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-04-29 19:25:46 +0200

    add-draft-dataloaders

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import pathlib
+from binance import enums
 
 # Paths
 BASE_PATH = str(pathlib.Path(__file__).parent)
@@ -7,3 +8,6 @@ BASE_PATH = str(pathlib.Path(__file__).parent)
 ASSET_TO_TRADE = "ETH"
 PREDICTOR_ASSETS = ["BTC", "LTC", "XRP", "ADA", "BNB", "XRP"]
 FIAT_TO_TRADE = "USDT"
+
+#
+BINANCE_INTERVAL = enums.KLINE_INTERVAL_1MINUTE
