306ed6f756dec1acc867e61d81eb9d1ef5643090
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-04-29 15:12:07 +0200

    feat: add binance client (#1)

---
+++ constants.py
@@ -0,0 +1,9 @@
+import pathlib
+
+# Paths
+BASE_PATH = str(pathlib.Path(__file__).parent)
+
+# Assets
+ASSET_TO_TRADE = "ETH"
+PREDICTOR_ASSETS = ["BTC", "LTC", "XRP", "ADA", "BNB", "XRP"]
+FIAT_TO_TRADE = "USDT"
