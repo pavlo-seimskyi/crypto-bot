579e58d68686225804bda0afd4201d41b381341b
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-04-29 12:31:23 +0200

    add-basic-binance-client

---
+++ clients/__init__.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from constants import BASE_PATH
+
+load_dotenv(os.path.join(BASE_PATH, ".env"))
+
+ENV = os.environ.copy()
+
+__all__ = ["ENV"]
---
+++ clients/binance.py
@@ -0,0 +1,49 @@
+from binance.client import Client
+
+
+class BinanceClient:
+    def __init__(self, api_key: str, api_secret: str):
+        """
+        Initialize BinanceClient instance.
+
+        Parameters
+        ----------
+        api_key : str
+            API key for Binance.
+        api_secret : str
+            API secret for Binance.
+        """
+        self.client = Client(api_key=api_key, api_secret=api_secret)
+
+    def get_historic_prices(
+        self,
+        symbol: str,
+        interval: str,
+        start_timestamp: int,
+        end_timestamp: int,
+    ):
+        """
+        Get historical exchange rates for a currency pair from Binance.
+
+        Parameters
+        ----------
+        symbol : str
+            Currency pair symbol, such as `ETHUSDT`.
+        interval : str
+            Time interval, one of `binance.enums`.
+        start_timestamp : int
+            Start timestamp.
+        end_timestamp : int
+            End timestamp.
+
+        Returns
+        -------
+        list
+            Historical exchange rates for the given currency pair.
+        """
+        return self.client.get_historical_klines(
+            symbol=symbol,
+            interval=interval,
+            start_str=start_timestamp,
+            end_str=end_timestamp,
+        )
