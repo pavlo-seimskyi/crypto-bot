5f5c2f677c91fcfdfde3551b3dbba7cdd49e193f
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-09 19:55:07 +0200

    feat: add lstm architecture (#14)

--- src/model/architectures/__init__.py
+++ src/model/architectures/__init__.py
@@ -1,5 +1,9 @@
+from src.model.architectures.lstm_binary import LSTMBinaryClassifier
 from src.model.architectures.transformer_binary import (
     TransformerBinaryClassifier,
 )
 
-__all__ = ["TransformerBinaryClassifier"]
+__all__ = [
+    "TransformerBinaryClassifier",
+    "LSTMBinaryClassifier",
+]
---
+++ src/model/architectures/lstm_binary.py
@@ -0,0 +1,28 @@
+import torch
+
+
+class LSTMBinaryClassifier(torch.nn.Module):
+    def __init__(
+        self, n_features: int, n_hidden: int, n_layers: int, n_out: int = 1
+    ):
+        super(LSTMBinaryClassifier, self).__init__()
+        self.n_hidden = n_hidden
+        self.n_layers = n_layers
+        # The LSTM layer
+        self.lstm = torch.nn.LSTM(
+            n_features, n_hidden, n_layers, batch_first=True
+        )
+        # The output layer
+        self.fc = torch.nn.Linear(n_hidden, n_out)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        # Initialize hidden and cell states
+        h0 = torch.zeros(self.n_layers, x.size(0), self.n_hidden)
+        c0 = torch.zeros(self.n_layers, x.size(0), self.n_hidden)
+        # Forward propagate the LSTM
+        out, _ = self.lstm(x, (h0, c0))
+        # Only take the output from the final timetep
+        out = self.fc(out[:, -1, :])
+        out = self.sigmoid(out)
+        return out.squeeze(dim=1)
