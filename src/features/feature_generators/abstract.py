705e14937007365ae82c20a27df3c3fa6a3c6861
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-09 14:43:59 +0200

    adjust types in abstract feature gen

--- src/features/feature_generators/abstract.py
+++ src/features/feature_generators/abstract.py
@@ -1,24 +1,22 @@
 from abc import ABC, abstractmethod, abstractproperty
-from typing import Any, Dict, List, Union
-
-from pandas import DataFrame, Series
+from typing import Any, Dict, List
 
 
 class FeatureGenerator(ABC):
     """Abstract class for feature generators."""
 
     @abstractmethod
-    def initialize(self, data: Union[DataFrame, Dict[str, List[Any]]]) -> None:
+    def initialize(self, data: Any) -> None:
         """Initialize the generator and its output values."""
         raise NotImplementedError()
 
     @abstractmethod
-    def add_value(self, data: Union[Series, Dict[str, Any]]) -> None:
+    def add_value(self, data: Any, purging: bool) -> None:
         """Add a new value to the feature generator."""
         raise NotImplementedError()
 
     @abstractproperty
-    def output_values(self) -> Dict[str, Any]:
+    def output_values(self) -> Dict[str, List[float]]:
         raise NotImplementedError()
 
     @abstractproperty
