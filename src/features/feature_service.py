dcc45fdc7f8d1ff3d5d53fa8cc64b976c243349b
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-09 14:38:22 +0200

    add types to feature service

--- src/features/feature_service.py
+++ src/features/feature_service.py
@@ -1,20 +1,22 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
+
+from src.features.feature_generators.abstract import FeatureGenerator
 
 
 class FeatureService:
-    def __init__(self, *feature_generators):
+    def __init__(self, *feature_generators: List[FeatureGenerator]):
         self.feature_generators = feature_generators
 
-    def initialize(self, data):
+    def initialize(self, data: Any) -> None:
         for feature_generator in self.feature_generators:
             feature_generator.initialize(data)
 
-    def add_value(self, data_row, purging=False):
+    def add_value(self, data_row: Any, purging: bool = False):
         for feature_generator in self.feature_generators:
             feature_generator.add_value(data_row, purging)
 
     @property
-    def output_values(self):
+    def output_values(self) -> Dict[str, List[float]]:
         return self.flatten_dict(
             {
                 feature_generator.name: feature_generator.output_values
