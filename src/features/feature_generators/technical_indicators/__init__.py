e0b265d8f390cca16186db0ca2a1d9fa8ccce26b
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-11 01:00:52 +0200

    add ATR indicator

--- src/features/feature_generators/technical_indicators/__init__.py
+++ src/features/feature_generators/technical_indicators/__init__.py
@@ -1,10 +1,11 @@
+from src.features.feature_generators.technical_indicators.atr import ATR
 from src.features.feature_generators.technical_indicators.bb import BB
+from src.features.feature_generators.technical_indicators.bbp import BBP
 from src.features.feature_generators.technical_indicators.ema import EMA
 from src.features.feature_generators.technical_indicators.macd import MACD
 from src.features.feature_generators.technical_indicators.obv import OBV
 from src.features.feature_generators.technical_indicators.rsi import RSI
 from src.features.feature_generators.technical_indicators.sma import SMA
-from src.features.feature_generators.technical_indicators.bbp import BBP
 
 __all__ = [
     "EMA",
@@ -14,4 +15,5 @@ __all__ = [
     "MACD",
     "RSI",
     "BBP",
+    "ATR",
 ]
---
+++ src/features/feature_generators/technical_indicators/atr.py
@@ -0,0 +1,56 @@
+import numpy as np
+from talipp.indicators import ATR as ATR_talipp
+from talipp.ohlcv import OHLCVFactory
+
+from src.features.feature_generators.abstract import FeatureGenerator
+
+
+class ATR(FeatureGenerator):
+    """
+    Average True Range.
+    """
+
+    def __init__(
+        self, high_col: str, low_col: str, close_col: str, period: int = 14
+    ) -> None:
+        super().__init__()
+        self.talipp_instance = None
+        self.high_col = high_col
+        self.low_col = low_col
+        self.close_col = close_col
+        self.period = period
+
+    def initialize(self, data):
+        input_values = OHLCVFactory.from_dict(
+            {
+                "high": data[self.high_col],
+                "low": data[self.low_col],
+                "close": data[self.close_col],
+            }
+        )
+        self.talipp_instance = ATR_talipp(
+            input_values=input_values,
+            period=self.period,
+        )
+
+    def add_value(self, data, purging: bool = False):
+        new_value = OHLCVFactory.from_dict(
+            {
+                "high": data[self.high_col],
+                "low": data[self.low_col],
+                "close": data[self.close_col],
+            }
+        )
+        self.talipp_instance.add_input_value(new_value)
+        if purging is True:
+            self.talipp_instance.purge_oldest(1)
+
+    @property
+    def output_values(self):
+        assert self.talipp_instance is not None, "Initialize first."
+        nans = (self.period - 1) * [np.nan]
+        return nans + self.talipp_instance.output_values
+
+    @property
+    def name(self):
+        return f"ATR__{self.input_col}__{self.period}"
