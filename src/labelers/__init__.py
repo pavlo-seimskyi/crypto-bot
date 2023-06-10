cf38ca42ea64fd6e9b3231c7045d12f05113615d
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-11 01:01:16 +0200

    add binary smooth labeler

--- src/labelers/__init__.py
+++ src/labelers/__init__.py
@@ -1,4 +1,5 @@
 from src.labelers.binary_labeler import BinaryLabeler
+from src.labelers.binary_smooth_labeler import BinarySmoothLabeler
 from src.labelers.three_bar_labeler import ThreeBarLabeler
 
-__all__ = ["BinaryLabeler", "ThreeBarLabeler"]
+__all__ = ["BinaryLabeler", "ThreeBarLabeler", "BinarySmoothLabeler"]
