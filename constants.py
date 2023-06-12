f5ac859a18b30260f0469d8783425bc1a2e9a0d5
Pavlo Seimskyi <62653730+pavlo-seimskyi@users.noreply.github.com>
2023-06-12 20:13:53 +0200

    remove XRP from asset list

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ BASE_PATH = str(pathlib.Path(__file__).parent)
 
 # Assets
 ASSET_TO_TRADE = "ETH"
-PREDICTOR_ASSETS = ["BTC", "LTC", "XRP", "ADA", "BNB"]
+PREDICTOR_ASSETS = ["BTC", "LTC", "ADA", "BNB"]  # "XRP", 
 FIAT_TO_TRADE = "USDT"
 
 BINANCE_INTERVAL = enums.KLINE_INTERVAL_1MINUTE
